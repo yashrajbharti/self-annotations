022c4a33f6163e5f287bf32da45867491b948c9a
Yash Raj Bharti <yashrajbharti.met18@itbhu.ac.in>
2021-12-11 00:37:32 +0530

    self placed annotations

--- tap-place.js
+++ tap-place.js
@@ -24,12 +24,12 @@ const newElement = document.createElement('a-entity')
  checkbox.addEventListener('change', function () {
    if (checkbox.checked) {
      // do this
-     newElement.setAttribute('rotation', '0 0 0')
+     newElement.setAttribute('rotation', '0 0 -90')
      place.setAttribute('rotation', '0 0 90')
      newElement.parentNode.removeChild()
    } else {
      // do that
-     newElement.setAttribute('rotation', '0 0 0')
+     newElement.setAttribute('rotation', '0 0 90')
      place.setAttribute('rotation', '0 0 0')
      newElement.parentNode.removeChild()
    }

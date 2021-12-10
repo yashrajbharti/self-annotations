b45b287f1c595b6e5d96009c9b80fdab8e93e118
Yash Raj Bharti <yashrajbharti.met18@itbhu.ac.in>
2021-12-10 22:40:57 +0530

    self placed annotations

--- tap-place.js
+++ tap-place.js
@@ -35,7 +35,7 @@ if (place.hasChildNodes()) {
     // It has at least one
 remove.addEventListener('click', (event) => {
   console.log(place.childNodes.length);
-  place.removeChild(place.childNodes.length - 1)
+  place.removeChild(place.childNodes[place.childNodes.length - 1])
 
 });
 }

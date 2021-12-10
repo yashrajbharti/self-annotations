e5b8ab997ba59ff4b0df551761cdea62a627d23a
Yash Raj Bharti <yashrajbharti.met18@itbhu.ac.in>
2021-12-10 21:19:30 +0530

    self placed annotations

--- tap-place.js
+++ tap-place.js
@@ -17,7 +17,7 @@ const newElement = document.createElement('a-entity')
 
     place.appendChild(newElement)
     // touchPoint.x  /= 1000;
-    // touchPoint.y  /= 2000;
+    touchPoint.y  -= -0.3;
     // touchPoint.z  /= 200000;
 
     newElement.setAttribute('position', touchPoint)

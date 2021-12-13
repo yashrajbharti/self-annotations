6a4b8e9de3bf4f2c4e8fcb67451197acfa502ed4
Yash Raj Bharti <yashrajbharti.met18@itbhu.ac.in>
2021-12-13 21:16:06 +0530

    self placed annotations

--- localstorage.js
+++ localstorage.js
@@ -1,14 +1,12 @@
-toggleswitch.addEventListener('change', () => {
-    localStorage.setItem("toggleswitch", toggleswitch.checked);
-    localStorage.setItem("Landscape", "Landscape");
-    console.log(localStorage)
-  });
-
-  function isChecked() {
-
-    document.getElementById('toggleswitch').checked = localStorage.getItem('toggleswitch') === "true";
-    document.getElementById('status').innerHTML = localStorage.getItem('Landscape');
-  }
-  window.onload = function() {
-   isChecked();
- };
+// toggleswitch.addEventListener('change', () => {
+//     localStorage.setItem("toggleswitch", toggleswitch.checked);
+//   });
+//
+//   function isChecked() {
+//
+//     document.getElementById('toggleswitch').checked = localStorage.getItem('toggleswitch') === "true";
+//     document.getElementById('status').innerHTML = localStorage.getItem('orientation');
+//   }
+//   window.onload = function() {
+//    isChecked();
+//  };

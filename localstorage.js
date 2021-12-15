c0cc44f8e9da570c7291f4f838f0547833d63265
Yash Raj Bharti <yashrajbharti.met18@itbhu.ac.in>
2021-12-15 06:28:05 +0530

    self placed annotations

--- localstorage.js
+++ localstorage.js
@@ -5,7 +5,6 @@
 //   function isChecked() {
 //
 //     document.getElementById('toggleswitch').checked = localStorage.getItem('toggleswitch') === "true";
-//     document.getElementById('status').innerHTML = localStorage.getItem('orientation');
 //   }
 //   window.onload = function() {
 //    isChecked();
